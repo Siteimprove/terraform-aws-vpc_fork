782537755f21d31c32e710b6178e06ae2fea3a96
Nick Letellier <nle@siteimprove.com>
2023-10-02 16:05:12 +0100

    Fix: remove autogenerated ipv6 commands

--- main.tf
+++ main.tf
@@ -28,15 +28,15 @@ locals {
 resource "aws_vpc" "this" {
   count = local.create_vpc ? 1 : 0
 
-  cidr_block          = var.use_ipam_pool ? null : var.cidr
+  cidr_block          = var.use_ipam_pool && !var.ipv4_ipam_pool_id ? null : var.cidr
   ipv4_ipam_pool_id   = var.ipv4_ipam_pool_id
   ipv4_netmask_length = var.ipv4_netmask_length
 
-  assign_generated_ipv6_cidr_block     = null
+  # assign_generated_ipv6_cidr_block     = var.enable_ipv6 && !var.use_ipam_pool ? true : null
   ipv6_cidr_block                      = var.ipv6_cidr
   ipv6_ipam_pool_id                    = var.ipv6_ipam_pool_id
   ipv6_netmask_length                  = var.ipv6_netmask_length
-  ipv6_cidr_block_network_border_group = var.ipv6_cidr_block_network_border_group
+  # ipv6_cidr_block_network_border_group = var.ipv6_cidr_block_network_border_group
 
   instance_tenancy                     = var.instance_tenancy
   enable_dns_hostnames                 = var.enable_dns_hostnames
